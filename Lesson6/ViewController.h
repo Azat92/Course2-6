82e8b62f2ca5f27648b8468e876de20dee096b0a
Azat Almeev <azat.almeev@gmail.com>
2015-10-09 18:06:16 +0300

    - Lesson

--- Lesson6/ViewController.h
+++ Lesson6/ViewController.h
@@ -8,8 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
-
+@interface ViewController : UITableViewController
+@property (nonatomic) NSMutableArray *items;
 @end
 
--- Lesson6/ViewController.m
+++ Lesson6/ViewController.m
@@ -7,21 +7,37 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
-@end
+#import "SecondViewController.h"
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.items = [@[ @0 ] mutableCopy];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)returnHere:(UIStoryboardSegue *)sender {
+    SecondViewController *svc = sender.sourceViewController;
+    NSInteger count = [svc.label.text integerValue];
+    if (self.items.count > count)
+        [self.items removeObjectsInRange:NSMakeRange(count, self.items.count - count)];
+    else {
+        NSInteger delta = count - self.items.count;
+        for (NSInteger i = 0; i < delta; i++)
+            [self.items addObject:@(self.items.count)];
+    }
+    [self.tableView reloadData];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.items.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *identifier = @"cellId";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@", @(indexPath.row)];
+    return cell;
 }
 
 @end
